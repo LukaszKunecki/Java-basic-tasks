87427cb35aa84c832d9d116158d657b609309a6f
Lukasz <lukaszkunecki7@gmail.com>
2023-10-25 00:01:17 +0200

    New file structure

---
+++ Java-Strings/Java-Strings/src/main/java/Tasks/Main.java
@@ -0,0 +1,36 @@
+package Tasks;
+
+public class Main {
+    public static void main(String[] args) {
+
+//      Create a function that will reverse the string (e.g. "Hello" becomes "olleH").
+//
+//      Create a function that will count the number of occurrences of a given character in a string
+//
+//      Create a function that will remove spaces from a given string.
+//
+//      Create a function that will check whether the string is a palindrome.
+//
+//      Create a function that will convert the first letter of each word in the string to upper case
+//      (e.g. "hello world" becomes "Hello World").
+//
+//      Create a function that will check whether two strings are anagrams (consist of the same letters).
+//
+//      Create a function that will remove all occurrences of a specific character from a string.
+//
+//      Create a function that will detect whether a string contains only numbers.
+//
+//      Create a function that will split a string based on a specified separator and return an array of parts.
+//
+//      Create a function that will convert all letters in a string to lowercase.
+//
+//      Create a function that returns the reversed order of words in a sentence (e.g. "Hello World" becomes "World Hello").
+//
+//      Create a function that will remove all special characters (e.g. periods, commas) from a string.
+//
+//      Create a function that returns the first non-repeating character in a string.
+//
+//      Create a function that will remove all consonants from a string.
+
+    }
+}
