414c14f5db6376fbab2267b4b02d70f7b4e002e5
Lukasz <lukaszkunecki7@gmail.com>
2023-11-05 21:09:54 +0100

    -

--- Java-Arrays/Java-Arrays/src/main/java/Implementation/Methods.java
+++ Java-Arrays/Java-Arrays/src/main/java/Implementation/Methods.java
@@ -1,7 +1,51 @@
 package Implementation;
 
+import java.util.Arrays;
+
 public class Methods {
 
+    public static int arraySum (int[] array){
+        int sum = 0;
+        for (int i = 0; i<array.length; i++){
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public static int largestElement (int[] array){
+        int largest = 0;
+        for (int element : array){
+            if (element > largest){
+                largest = element;
+            }
+        }
+        return largest;
+    }
+
+    public static float averageFloat (float[] array){
+        float sum = 0;
+        for (float element : array) {
+            sum += element;
+        }
+        return sum/array.length;
+    }
+
+    public static int[] reverseOrder (int[] array){
+        int[] newArray = new int[array.length];
+        for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+            newArray[j] = array[i];
+        }
+        return newArray;
+    }
+
+    public static boolean isPresent (int[] array, int target){
+        for (int element : array) {
+            if (element == target){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 }
--- Java-Arrays/Java-Arrays/src/main/java/Tasks/Main.java
+++ Java-Arrays/Java-Arrays/src/main/java/Tasks/Main.java
@@ -1,17 +1,37 @@
 package Tasks;
 
+import Implementation.Methods;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
 //      Create an array of integers and calculate the sum of all its elements.
+        System.out.println("Task nr 1");
+        int[] exampleArray1 = {5,3,7,6,5,9,8,2,1,4,3,6,7};
+        System.out.println(Methods.arraySum(exampleArray1));
 
 //      Find the largest element in a given array of integers.
+        System.out.println("Task nr 2");
+        int[] exampleArray2 = {5,3,7,6,5,9,8,2,1,4,3,6,7};
+        System.out.println(Methods.largestElement(exampleArray2));
 
-//      Calculate the arithmetic mean of the elements in an array of floating point numbers.
+//      Calculate the average of the elements in an array of floating point numbers.
+        System.out.println("Task nr 3");
+        float[] exampleArray3 = {5.4F,3.7F,7.2F,6.8F,5.1F,9.3F,8.2F,2.9F,1.0F,4.6F,3.5F,6.1F,7.3F};
+        System.out.println(Methods.averageFloat(exampleArray3));
 
 //      Reverse the order of elements in a given array.
+        System.out.println("Task nr 4");
+        int [] exampleArray4 = {5,3,56,8,9,1,3};
+        System.out.println(Arrays.toString(Methods.reverseOrder(exampleArray4)));
 
-//      Check if a given integer is in the array.
+//      Check if a given integer is present in the array.
+        System.out.println("Task nr 5");
+        int targetInteger = 7;
+        int[] exampleArray5 = {5,3,7,6,5,9,8,2,1,4,3,6,7};
+        System.out.println(Methods.isPresent(exampleArray5, targetInteger));
 
 //      Remove duplicates from a given integer array.
 
