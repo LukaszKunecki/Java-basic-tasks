7b90fd7c7d216eed6d44b392de7fa8b092548447
Lukasz <lukaszkunecki7@gmail.com>
2023-11-12 00:27:47 +0100

    -

---
+++ Java-Collections/Java-Collections/src/main/java/Tasks/Main.java
@@ -0,0 +1,25 @@
+package Tasks;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//      Create a list of integers and add 5 items to it. Then list all the items on your list.
+//
+//      Create a map that assigns month names to their numbers.
+//      Add some entries and get the month number based on the name.
+//
+//      Make a list of 10 integers. Remove every other item from the list.
+//
+//      Create a map of pairs of countries and their capitals.
+//      Then use a loop to iterate through the map and display all country-capital pairs.
+//
+//      Create a list of strings representing people's names. Sort this list alphabetically.
+//
+//      Create a set of integers. Remove all even numbers from it.
+//
+//      Create a map that assigns products to their prices.
+//      Write code that checks whether a given product is on the map (if it is available) based on the product name.
+
+    }
+}
