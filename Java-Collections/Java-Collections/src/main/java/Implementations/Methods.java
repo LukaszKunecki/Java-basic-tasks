0aac00c1f9b1b62a8b725973cda8b4a1bb42b4b2
Lukasz <lukaszkunecki7@gmail.com>
2023-11-13 00:41:57 +0100

    -

--- Java-Collections/Java-Collections/src/main/java/Implementations/Methods.java
+++ Java-Collections/Java-Collections/src/main/java/Implementations/Methods.java
@@ -1,4 +1,32 @@
 package Implementations;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
 public class Methods {
+
+    public static int monthNumber (String monthName, Map<String, Integer> map){
+        int number = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getKey().equals(monthName)){
+                number =  entry.getValue();
+                break;
+            }
+        }
+        return number;
+    }
+
+    public static List<Integer> deleteSecondElement (List<Integer> list){
+        for (int i=list.size()-1; i>0; i-=2){
+            list.remove(i);
+        }
+        return list;
+    }
+
+    public static List<String> sortnames(List<String> list){
+
+    }
+
 }
--- Java-Collections/Java-Collections/src/main/java/Tasks/Main.java
+++ Java-Collections/Java-Collections/src/main/java/Tasks/Main.java
@@ -1,20 +1,28 @@
 package Tasks;
 
+import Implementations.Methods;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
-
-//      Create a list of integers and add 5 items to it. Then list all the items on your list.
-//
 //      Create a map that assigns month names to their numbers.
-//      Add some entries and get the month number based on the name.
-//
-//      Make a list of 10 integers. Remove every other item from the list.
-//
-//      Create a map of pairs of countries and their capitals.
-//      Then use a loop to iterate through the map and display all country-capital pairs.
+//      Add some month and numbers and get the month number based on the name.
+        System.out.println("Task nr 1");
+        Map<String, Integer> monthMap = new HashMap<>(){{put("January", 1);put("February", 2);put("March", 3);}};
+        String targetMonth = "March";
+        System.out.println("Value assigned to "+ targetMonth +" is: "+ Methods.monthNumber(targetMonth, monthMap));
+
+//      Make a list of 10 integers. Remove every second item from the list.
+        System.out.println("Task nr 2");
+        List<Integer> exampleList1 = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9,10));
+        System.out.println(Methods.deleteSecondElement(exampleList1));
 //
 //      Create a list of strings representing people's names. Sort this list alphabetically.
+        System.out.println("Task nr 3");
+        List<String> namesList = new ArrayList<>(List.of("John", "Greg", "Anna", "Bruce", "Kate"));
+        System.out.println(Methods.sortnames(namesList));
 //
 //      Create a set of integers. Remove all even numbers from it.
 //
