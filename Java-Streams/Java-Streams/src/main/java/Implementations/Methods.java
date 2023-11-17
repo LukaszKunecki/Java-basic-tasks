148b6b067b71ec73ecfa0cb8f7085887e7d270da
Lukasz <lukaszkunecki7@gmail.com>
2023-11-18 00:23:43 +0100

    -

--- Java-Streams/Java-Streams/src/main/java/Implementations/Methods.java
+++ Java-Streams/Java-Streams/src/main/java/Implementations/Methods.java
@@ -1,4 +1,21 @@
 package Implementations;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Methods {
+    public static int integerSum(List<Integer> list){
+        return list.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public static double doubleAvg(List<Double> list){
+        return list.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+    }
+
+    public static List<String> filterString (List<String> list){
+        return list.stream().filter(e -> e.length()>3).toList();
+    }
+
+
+
 }
--- Java-Streams/Java-Streams/src/main/java/Tasks/Main.java
+++ Java-Streams/Java-Streams/src/main/java/Tasks/Main.java
@@ -1,18 +1,29 @@
 package Tasks;
 
+import Implementations.Methods;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
 //      Write a function that, using the Stream API, sums all integers from a given list.
         System.out.println("Task nr 1");
+        List<Integer> examplelist1 = new ArrayList<>(List.of(1,2,4,8,5,4,9,8,6,5,2,7));
+        System.out.println(Methods.integerSum(examplelist1));
 
 //      Write a function that, using the Stream API, finds the average value of numbers from a given list
-//      of floating-point numbers.
+//      of double-point numbers.
         System.out.println("Task nr 2");
+        List<Double> examplelist2 = new ArrayList<>(List.of(1.5D,2.4D,4.2D,8.0D,5.1D,4.9D,9.6D,8.7D,6.2D,5.5D));
+        System.out.println(Methods.doubleAvg(examplelist2));
 
 //      Write a function that, using the Stream API, filters all strings from a given
-//      list that have more than 5 characters.
+//      list that have more than 3 characters.
         System.out.println("Task nr 3");
+        List<String> examplelist3 = new ArrayList<>(List.of("Hello", "World", "Test", "List", "Str"));
+        System.out.println(Methods.filterString(examplelist3));
 
 //      Write a function that, using the Stream API, finds the longest string in a given collection of strings.
         System.out.println("Task nr 4");
@@ -80,7 +91,7 @@ public class Main {
 //      Write a function that, using the Java Stream API, concatenates strings from a given list
 //      into one long string, adding a separator between them.
         System.out.println("Task nr 22");
-        
+
 
     }
 }
