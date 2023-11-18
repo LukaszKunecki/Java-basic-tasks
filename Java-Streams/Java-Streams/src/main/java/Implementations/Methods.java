05f21fc5609aca3cc871d2940e649584b4f815ae
Lukasz <lukaszkunecki7@gmail.com>
2023-11-19 00:08:07 +0100

    -

--- Java-Streams/Java-Streams/src/main/java/Implementations/Methods.java
+++ Java-Streams/Java-Streams/src/main/java/Implementations/Methods.java
@@ -1,9 +1,6 @@
 package Implementations;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Methods {
@@ -27,8 +24,51 @@ public class Methods {
         return list.stream().collect(Collectors.groupingBy(String::length));
     }
 
-    public static List<String> reverseString(List<String> list){
+    public static List<String> reverseString (List<String> list){
         return list.stream().map(e -> new StringBuilder(e).reverse().toString()).toList();
+    }
+
+    public static List<Integer> sortDesc (List<Integer> list){
+        return list.stream().sorted((a,b) -> Integer.compare(b,a)).toList();
+    }
+
+    public static int sumofSq (List<Integer> list){
+        return list.stream().map(number -> number*number).mapToInt(Integer::intValue).sum();
+    }
+
+    public static String firstString (List<String> list, char letter){
+        return list.stream().filter(string -> string.startsWith(String.valueOf(letter))).findFirst().toString();
+    }
+
+    public static List<Integer> evenNumbersOnly (List<Integer> list){
+         return list.stream().filter(number -> number % 2 == 0).toList();
+    }
+
+    public static String concatenateStrings (List<String> list){
+        return list.stream().collect(Collectors.joining("_"));
+    }
+
+    public static OptionalDouble averageLength (List<String> list){
+        return list.stream().mapToInt(String::length).average();
+    }
+
+    public static List<OptionalInt> minAndMax (List<Integer> list){
+        OptionalInt max = list.stream().mapToInt(Integer::intValue).max();
+        OptionalInt min = list.stream().mapToInt(Integer::intValue).min();
+        List<OptionalInt> minMax = new ArrayList<>(List.of(min,max));
+        return minMax;
+    }
+
+    public static List<String> uniqueElements (List<String> list){
+        return list.stream().distinct().toList();
+    }
+
+    public static List<String> wordsWithLetter (List<String> list, char target){
+       return list.stream().filter(str ->str.contains(String.valueOf(target))).toList();
+    }
+
+    public static String concatenateStringsNoSeparator (List<String> list){
+        return list.stream().collect(Collectors.joining());
     }
 
 }
--- Java-Streams/Java-Streams/src/main/java/Tasks/Main.java
+++ Java-Streams/Java-Streams/src/main/java/Tasks/Main.java
@@ -42,61 +42,79 @@ public class Main {
 
 //      Write a function that, using the Stream API, sorts integers in a given list in descending order.
         System.out.println("Task nr 7");
+        List<Integer> examplelist7 = new ArrayList<>(List.of(1,2,4,8,5,4,9,8,6,5,2,7));
+        System.out.println(Methods.sortDesc(examplelist7));
 
 //      Write a function that, using the Stream API, finds the sum of squares of numbers from a given list.
         System.out.println("Task nr 8");
+        List<Integer> examplelist8 = new ArrayList<>(List.of(9,6,4,18,5,4,9,18,6,3,2,8));
+        System.out.println(Methods.sumofSq(examplelist8));
 
 //      Write a function that, using the Stream API, finds the first string from a given list that
 //      starts with a specified letter.
         System.out.println("Task nr 9");
+        List<String> examplelist9 = new ArrayList<>(List.of("Hello", "World", "Test", "List", "Washington", "Windows"));
+        char targetLetter = 'W';
+        System.out.println(Methods.firstString(examplelist9, targetLetter));
 
 //      Write a function that, using the Stream API, selects only even numbers from a given list of integers.
         System.out.println("Task nr 10");
+        List<Integer> examplelist10 = new ArrayList<>(List.of(9,6,4,18,5,4,9,18,6,3,2,8));
+        System.out.println(Methods.evenNumbersOnly(examplelist10));
 
 //      Write a function that, using the Stream API, concatenates strings from a given list into one long string,
 //      separating them by a specified separator.
         System.out.println("Task nr 11");
+        List<String> examplelist11 = new ArrayList<>(List.of("Hello", "World", "Test"));
+        System.out.println(Methods.concatenateStrings(examplelist11));
 
 //      Write a function that, using the Stream API, calculates the average length of strings
 //      in a given collection of strings.
         System.out.println("Task nr 12");
+        List<String> examplelist12 = new ArrayList<>(List.of("Hello", "World", "Test", "List", "Washington", "Windows"));
+        System.out.println(Methods.averageLength(examplelist12));
 
 //      Write a function that, using the Stream API, finds the maximum and minimum values
 //      in a given collection of integers.
         System.out.println("Task nr 13");
+        List<Integer> examplelist13 = new ArrayList<>(List.of(9,6,4,18,5,4,9,18,6,3,2,8));
+        System.out.println(Methods.minAndMax(examplelist13));
 
 //      Write a function that, using the Java Stream API, selects only unique elements from a given list.
         System.out.println("Task nr 14");
+        List<String> examplelist14 = new ArrayList<>(List.of("Hello", "World", "Test", "Hello", "World", "Element"));
+        System.out.println(Methods.uniqueElements(examplelist14));
 
 //      Write a function that, using the Java Stream API, filters words in a given collection of strings
-//      that contain a specific word or letter.
+//      that contain a specific letter.
         System.out.println("Task nr 15");
+        List<String> examplelist15 = new ArrayList<>(List.of("Hello", "World", "Test", "Hello", "World", "Element"));
+        char targetChar = 's';
+        System.out.println(Methods.wordsWithLetter(examplelist15, targetChar));
 
 //      Write a function that, using the Java Stream API, concatenates strings from a given list into
 //      one long string without using a separator.
         System.out.println("Task nr 16");
-
-//      Write a function that, using the Java Stream API, finds the first element in a given collection
-//      that meets a specific condition.
-        System.out.println("Task nr 17");
+        List<String> examplelist16 = new ArrayList<>(List.of("Hello", "World", "Test", "Hello", "World", "Element"));
+        System.out.println(Methods.concatenateStringsNoSeparator(examplelist16));
 
 //      Write a function that, using the Java Stream API, calculates the sum and product of elements
 //      in a given collection of integers.
-        System.out.println("Task nr 18");
+        System.out.println("Task nr 17");
 
 //      Write a function that, using the Java Stream API, selects only unique elements from a given list.
-        System.out.println("Task nr 19");
+        System.out.println("Task nr 18");
 
 //      Write a function that, using the Java Stream API, filters elements in a given collection
 //      that meet a specific condition, for example, selects only numbers greater than 10.
-        System.out.println("Task nr 20");
+        System.out.println("Task nr 19");
 
 //      Write a function that, using the Java Stream API, sorts strings in a given collection by their length.
-        System.out.println("Task nr 21");
+        System.out.println("Task nr 20");
 
 //      Write a function that, using the Java Stream API, concatenates strings from a given list
 //      into one long string, adding a separator between them.
-        System.out.println("Task nr 22");
+        System.out.println("Task nr 21");
 
 
     }
